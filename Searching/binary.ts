8c364945054d98906742abab00d5f445c4e34457
Anoopprep <prepeu1223@gmail.com>
2025-10-26 13:09:27 +0530

    Binary search

---
+++ Searching/binary.ts
@@ -0,0 +1,17 @@
+// Only for sorted arrays
+// Given a sorted array and asked to find a target in the array, try using binary search
+// or modified binary search to get the solution in O(logn)
+
+function binary(arr: Array<number>, target: number) {
+    let i = 0, j = arr.length - 1;
+    while (i <= j) {
+        let mid = Math.floor((i+j)/2);
+        if(arr[mid] === target) return mid;
+        if(target < arr[mid]) j = mid - 1;
+        else i = mid + 1;
+    }
+    return -1
+}
+
+
+console.log(binary([3,4,5,6,7,8,9], 9));
