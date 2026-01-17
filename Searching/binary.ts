99d6ab9b6691588ea568222072b271094910c24e
Anoopprep <prepeu1223@gmail.com>
2026-01-17 18:23:15 +0530

    Add comment for mid calculation in binary search
    Added a comment to explain the calculation of mid in binary search to avoid overflow.

--- Searching/binary.ts
+++ Searching/binary.ts
@@ -1,6 +1,7 @@
 // Only for sorted arrays
 // Given a sorted array and asked to find a target in the array, try using binary search
 // or modified binary search to get the solution in O(logn)
+// mid = low + (high - low) / 2; do this to avoid overflow, this will avoid overflow because it's value will always be less than the size of the array
 
 function binary(arr: Array<number>, target: number) {
     let i = 0, j = arr.length - 1;
