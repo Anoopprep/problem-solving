8f725095e7aa29affb828c3d38f58cb98c598fe9
Anoopprep <prepeu1223@gmail.com>
2026-04-08 18:46:52 +0530

    Update IsSubsequence.java

--- Strings/IsSubsequence.java
+++ Strings/IsSubsequence.java
@@ -22,4 +22,19 @@ class IsSubsequence {
         }
         return true;
     }
+
+    public boolean isSubsequenceRecursive(String s, String t) {
+        return helper(s, t, 0, 0);
+    }
+
+    private boolean helper(String s, String t, int i, int j){
+        if(i == s.length() && j == t.length()) return true;
+        if(i == s.length()) return true;
+        if(j == t.length()) return false;
+
+        if(s.charAt(i) == t.charAt(j)) 
+            return helper(s, t, i+1, j+1);
+        else 
+            return helper(s, t, i, j+1);
+    }
 }
